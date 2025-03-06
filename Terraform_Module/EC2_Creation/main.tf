1694c6e73d8354cf9556c138437eec35639a76f0
nithinkkumar95 <nithinkkumar@gmail.com>
2025-03-06 18:07:00 +0530

    Update main.tf

--- Terraform_Module/EC2_Creation/main.tf
+++ Terraform_Module/EC2_Creation/main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_vpc.main.id
+  subnet_id     = var.subnet_cidr
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
